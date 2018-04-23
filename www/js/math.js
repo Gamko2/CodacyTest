e7cb199c6e1b1ecc915ad334ce93f477c8d2af40
Kelvin <test@test.com>
2018-04-23 13:12:25 +0200

    example testing

---
+++ www/js/math.js
@@ -0,0 +1,12 @@
+function addition(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+      return a - b;
+    }
+  
+  module.exports = {
+    addition: addition,
+    sub: subtract
+  }
