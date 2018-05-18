ac06e234d89921fc1707db08a38e86ccb0d81dcb
DESKTOP-GNESREK\Michael Korn <tachyon107@web.de>
2018-05-18 15:25:02 +0200

    toast.js manuell importiert von "FW7&MDDeisgn" hinzugefügt

---
+++ www/js/toast.js
@@ -0,0 +1,15 @@
+// Create top toast
+
+var toastTop = undefined;
+
+
+function displayToastMessage(message) {
+    toastTop = app.toast.create({
+        text: message,
+        position: 'top',
+        closeTimeout: 2000,
+
+
+    });
+    toastTop.open();
+}
